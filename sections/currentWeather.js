c2442e18d5e5d8b1019c1951a2c11ca339781255
Ale Nica <alexandrahetea99@gmail.com>
2023-02-28 17:04:04 +0200

    stilizare iconita grade

--- sections/currentWeather.js
+++ sections/currentWeather.js
@@ -40,14 +40,14 @@ async function displayCurrentWeather(city) {
           <div class="fs-2 mb-2"><strong>${name}</strong></div>
           <div class="f2-3"><strong>${day}</strong>, ${hours}</div>
           <div class="d-flex align-items-center justify-content-center">
-            <strong class="fs-1">${temperature} °C</strong>
+            <strong class="fs-1">${temperature}°C</strong>
             <img src="${weatherIcon}" alt="" />
           </div>
         </div>
         <div class="px-3">
-          <p class="fs-5">Se simte ca <strong>${realFeel} °C</strong></p>
+          <p class="fs-5">Se simte ca <strong>${realFeel}°C</strong></p>
           <p class="fs-5">${weatherDescription}</p>
-          <p class="fs-5">Viteza vantului: <strong>${windSpeed} km/h</strong></p>
+          <p class="fs-5">Viteza vantului: <strong>${windSpeed}km/h</strong></p>
         </div>
       </div>`
     };
