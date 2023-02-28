3f9366ae4184d6e08abdb4780f09fb20c7e68d90
Ale Nica <alexandrahetea99@gmail.com>
2023-02-28 15:01:28 +0200

    added scroll to top button

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 const currentCity = localStorage.getItem("city");
 console.log("current", currentCity);
 
